3368ac5f25248d447731ed17692ed82fc15b180c
Tamir Bahar <tamir@north-bit.com>
2016-03-10 15:10:05 +0200

    Stil a bit hacky, but way more configurable.

--- ms_scraper/ms_scraper/pipelines.py
+++ ms_scraper/ms_scraper/pipelines.py
@@ -10,19 +10,30 @@ import scrapy.pipelines.files
 from scrapy.exceptions import DropItem
 import os
 import scrapy
+import errno
 
 SYMCHK_PATH = r'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\symchk.exe'
 SYM_PATH = r'SRV*c:\temp\symbols*https://msdl.microsoft.com/download/symbols'
 
+
 def expand(source, dest, filter_='*'):
     subprocess.call(['expand', '-F:{}'.format(filter_), source, dest])
 
-def symchk(path, symchk_path=None, sym_path=None):
+def delete(path):
+    subprocess.call(['del', '/Q', path], shell=True)
+
+def symchk(path, symchk_path=None, sym_path=None, output_dir=None):
     if symchk_path is None:
         symchk_path = SYMCHK_PATH
     if sym_path is None:
         sym_path = SYM_PATH
-    subprocess.call([symchk_path, '/r', path, '/s', sym_path, ])
+
+    output_dir_args = []
+    if output_dir is not None:
+        output_dir_args = ['/oc', output_dir]
+
+    subprocess.call([symchk_path, '/r', path, '/s', sym_path, ] + output_dir_args)
+
 
 class MsuDownloadPipeline(scrapy.pipelines.files.FilesPipeline):
     def get_media_requests(self, item, info):
@@ -40,7 +51,7 @@ class MsuDownloadPipeline(scrapy.pipelines.files.FilesPipeline):
 
     def file_path(self, request, response=None, info=None):
         bulletin = request.meta['bulletin'].upper()
-        path = os.path.join(bulletin, request.url.rsplit('/',1)[-1])
+        path = os.path.join(bulletin, request.url.rsplit('/', 1)[-1])
         return path
 
 
@@ -50,14 +61,47 @@ class MsuExtractPipeline(object):
         msu_dir = os.path.dirname(msu_path)
         msu_name = item['url'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
         extract_dir = os.path.join(msu_dir, msu_name)
-        os.mkdir(extract_dir)
+        try:
+            os.mkdir(extract_dir)
+        except WindowsError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
-        #TODO: Remove rubbish files.
         extract_cab = '{}.cab'.format(msu_name)
         expand(msu_path, extract_dir, extract_cab)
         filter_ = spider.settings.get('EXTRACT_FILTER', None)
         expand(os.path.join(extract_dir, extract_cab), extract_dir, filter_=filter_)
+
+        if spider.settings.get('DELETE_RUBBISH', False):
+            # Delete all files that are not in directories. This includes a lot of rubbish files, as well as the
+            # original `.cab` file.
+            delete(os.path.join(extract_dir, '*'))
+
+        if not spider.settings.get('DONT_DOWNLOAD_SYMBOLS', False):
+            self.download_symbols(extract_dir, spider)
+
+        if spider.settings.get('DELETE_MSU_FILES', False):
+            try:
+                os.unlink(msu_path)
+            except WindowsError:
+                pass
+
+        if spider.settings.get('DELETE_EMPTY_DIRS', True):
+            drop_item = False
+            if not os.listdir(extract_dir):
+                os.rmdir(extract_dir)
+                drop_item = True
+
+            if not os.listdir(msu_dir):
+                os.rmdir(msu_dir)
+
+            if drop_item:
+                raise DropItem('No item found.')
+
+        return item
+
+    def download_symbols(self, extract_dir, spider):
         symchk_path = spider.settings.get('SYMCHK_PATH', None)
         sym_path = spider.settings.get('SYM_PATH', None)
-        symchk(extract_dir, symchk_path=symchk_path, sym_path=sym_path)
-        return item
+        output_dir = spider.settings.get('DOWNLOAD_SYMBOLS_TO', None)
+        symchk(extract_dir, symchk_path=symchk_path, sym_path=sym_path, output_dir=output_dir)
--- ms_scraper/ms_scraper/settings.py
+++ ms_scraper/ms_scraper/settings.py
@@ -15,81 +15,29 @@ SPIDER_MODULES = ['ms_scraper.spiders']
 NEWSPIDER_MODULE = 'ms_scraper.spiders'
 
 
-PLATFORM_LIST = ['Windows 7 for 32-bit Systems Service Pack 1']
-EXTRACT_FILTER = 'rpcrt4.dll'
+PLATFORM_LIST = None
+EXTRACT_FILTER = 'ntdll.dll'
+
+## Set `DONT_DOWNLOAD_SYMBOLS` to `True` to prevent downloading symbols.
+DONT_DOWNLOAD_SYMBOLS = False
 
 SYMCHK_PATH = r'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\symchk.exe'
 SYM_PATH = r'SRV*c:\temp\symbols*https://msdl.microsoft.com/download/symbols'
 
+# Set a path to download symbols to. Otherwise - uses what is in `SYM_PATH`.
+DOWNLOAD_SYMBOLS_TO = r'c:\temp\syms'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ms_scraper (+http://www.yourdomain.com)'
+# Delete `.msu` files after extraction
+DELETE_MSU_FILES = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS=32
+# Delete rubbish files (the `.msu` files are full of them!)
+DELETE_RUBBISH = True
 
-# Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN=16
-#CONCURRENT_REQUESTS_PER_IP=16
+# Delete empty directories
+DELETE_EMPTY_DIRS = True
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED=False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED=False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'ms_scraper.middlewares.MyCustomSpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'ms_scraper.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
-
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    # 'scrapy.pipelines.files.FilesPipeline' : 300,
     'ms_scraper.pipelines.MsuDownloadPipeline' : 300,
     'ms_scraper.pipelines.MsuExtractPipeline' : 500,
 }
 FILES_STORE = r'C:\Temp\scraped'
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-# NOTE: AutoThrottle will honour the standard settings for concurrency and delay
-#AUTOTHROTTLE_ENABLED=True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY=5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY=60
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG=False
-
-# Enable and configure HTTP caching (disabled by default)
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED=True
-#HTTPCACHE_EXPIRATION_SECS=0
-#HTTPCACHE_DIR='httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES=[]
-#HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
